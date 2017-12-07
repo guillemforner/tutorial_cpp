6e6156e02528a7e6f208e5a963d7a2492ed2650d
Guillem Gelabert <guillem.forner@gmail.com>
2017-12-07 07:39:45 +0100

    Arrays

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -2,9 +2,13 @@
 
 using namespace std;
 
+void printArray(double[], int);
+void userInputArray(double[], int);
+
 int main()
 {
 	double rainfall[5];
+
 
 	rainfall[0] = 2.3;
 	rainfall[1] = 0.3;
@@ -12,20 +16,35 @@ int main()
 	rainfall[3] = 4.1;
 	rainfall[4] = 0.5;
 
-	for(int i = 0; i < 5; i++)
-	{
-		cout << rainfall[i] << endl;
-	}
+	printArray(rainfall, 5);
 
-	for(int i = 0; i < 5; i++)
+	userInputArray(rainfall, 5);
+
+	printArray(rainfall, 5);
+
+	return 0;
+}
+
+
+void printArray(double array[], int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		cout << array[i] << endl;
+	}
+}
+
+void userInputArray(double array[], int size)
+{
+	for(int i = 0; i < size; i++)
 	{
 		cout << "Enter a rainfall amount: ";
-		cin >> rainfall[i];
+		cin >> array[i];
 	}
-
-	for(int i = 0; i < 5; i++)
-	{
-		cout << rainfall[i] << endl;
-	}
-
 }
+
+
+
+
+
+

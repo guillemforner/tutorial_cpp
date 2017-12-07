30383f6d3161b08e5178c9c62427d67904d66586
Guillem Gelabert <guillem.forner@gmail.com>
2017-12-07 07:50:44 +0100

    Average

---
+++ Average/average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+double average(int array[], int size)
+{
+	double sum = 0.0;
+
+	for(int i = 0; i < size; i++)
+	{
+		sum += array[i];
+	}
+
+	return (sum / size);
+}
+
+int main()
+{
+	int points[] = {18, 20, 5, 17, 22};
+
+	cout << average(points, 5) << endl;
+
+	return 0;
+}
